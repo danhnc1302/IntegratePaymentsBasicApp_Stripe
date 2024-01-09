2df3cbfebf3aaa2a230293c8e21d71096c221b43
danhnc <danhnc1302@gmail.com>
2024-01-09 22:50:37 +0700

    modify UI

--- App.js
+++ App.js
@@ -2,124 +2,102 @@ import React, { useState } from 'react';
 import {
   Text,
   View,
-  Dimensions,
   Image,
-  TextInput,
-  Pressable
+  StyleSheet,
+  Alert,
+  StatusBar,
+  TouchableOpacity
 } from 'react-native';
-
-const _WIDTH = Dimensions.get("screen").width
+import { CreditCardInput } from "react-native-credit-card-input";
 
 export default function App() {
 
-  const [input, setInput] = useState("...")
+  const [CardInput, setCardInput] = useState({})
 
-  const handleInput = (value) => {
-    if (value.length === 0) {
-      setInput("");
-      return;
-    }
-    const newString = value.replace(/([^ ]{4}(?=[^ ]|$))/g, '$1 ');
-    setInput(newString.trim());
+  const _onChange = (data) => {
+    setCardInput(data)
   }
 
 
+  const onSubmit = () => {
+    if (CardInput.valid == false || typeof CardInput.valid == 'undefined') {
+      Alert.alert("Invalid Credit Card")
+      return false
+    }
+
+  }
+
+
+
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-    }}>
-      <View
-        style={{
-          width: 0.9 * _WIDTH,
-          aspectRatio: 1.5,
-          backgroundColor: "orange",
-          borderRadius: 10,
-          overflow: "hidden"
-        }}>
-        <Image source={require("./background.jpg")} style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "fill",
-        }} />
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#2471A3" />
+      <Image
+        source={{ uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Stripe_logo%2C_revised_2016.png/1200px-Stripe_logo%2C_revised_2016.png' }}
+        style={styles.ImgStyle}
+      />
+      <CreditCardInput
+        inputContainerStyle={styles.inputContainerStyle}
+        inputStyle={styles.inputStyle}
+        labelStyle={styles.labelStyle}
+        validColor="#fff"
+        placeholderColor="#ccc"
+        onChange={_onChange} />
 
-        <View style={{
-          position: "absolute",
-          top: 0,
-          right: 10,
-          flexDirection: "row",
-          alignItems: "center"
-        }}>
-          <Image source={require("./logo.png")} style={{ width: 80, height: 80, objectFit: "cover" }} />
-          <Text style={{
-            color: "#1a048a",
-            fontWeight: "bold",
-            fontSize: 28,
-          }}>DANH'S BANK</Text>
-
-        </View>
-
-        <View style={{
-          position: "absolute",
-          bottom: 10,
-          width: "100%",
-          padding: 15
-        }}>
-          <Text style={{
-            marginBottom: 10,
-            fontSize: 22,
-            fontWeight: "bold"
-          }}>{input}</Text>
-          <View style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center"
-          }}>
-            <Text style={{
-              fontSize: 18,
-              fontWeight: "500"
-            }}>NGUYEN CONG DANH</Text>
-            <Image source={require('./visapicture.png')} style={{ width: 80, height: 40, objectFit: "fill" }} />
-          </View>
-        </View>
-      </View >
-
-      <View style={{
-        flexDirection: 'row',
-        alignItems: "center",
-        gap: 10,
-        marginTop: 30
-      }}>
-        <TextInput
-          placeholder='Account Number'
-          onChangeText={(val) => handleInput(val)}
-          value={input}
-          style={{
-            width: _WIDTH * 0.6,
-            borderWidth: 1,
-            borderColor: "black",
-            alignItems: 'center',
-            paddingVertical: 5,
-            paddingHorizontal: 10
-          }}
-        />
-        <Pressable style={{
-          width: 60,
-          padding: 10,
-          borderRadius: 4,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#1a048a"
-        }}>
-          <Text style={{
-            color: 'white',
-            fontSize: 18,
-            fontWeight: "600"
-          }}>Pay</Text>
-        </Pressable>
-      </View>
-
+      <TouchableOpacity
+        onPress={onSubmit}
+        style={styles.button}>
+        <Text
+          style={styles.buttonText}>
+          Pay Now
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+
+  },
+  ImgStyle: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'contain',
+    borderRadius: 8,
+  },
+  button: {
+    backgroundColor: '#2471A3',
+    width: 150,
+    height: 45,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 15,
+    color: '#f4f4f4',
+    fontWeight: 'bold',
+    textTransform: 'uppercase'
+  },
+  inputContainerStyle: {
+    backgroundColor: '#fff',
+    borderRadius: 5
+  },
+  inputStyle: {
+    backgroundColor: '#222242',
+    paddingLeft: 15,
+    borderRadius: 5,
+    color: '#fff'
+  },
+  labelStyle: {
+    marginBottom: 5,
+    fontSize: 12
+  }
+
+});
+
